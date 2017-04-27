cde4e29350b9a6eda76194ff3dafab926df7c0f4
remyauda <remyauda@yahoo.fr>
2017-04-27 22:49:25 +0200

    [ADD] Initial commit.

---
+++ appel_skype.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="description" content="site appel Skype" />
+<meta name="keywords" content="skype" />
+<meta name="author" content="auda">
+<meta charset="UTF-8">
+<link rel="stylesheet" href="layout1.css">
+<title>Page Skype</title>
+</head>
+<body>
+
+<!-- Liens utiles: 
+https://support.skype.com/fr/faq/FA99/comment-puis-je-ajouter-les-boutons-contact-me-contactez-moi-et-des-liens-uri-de-skype-sur-mon-site-web-ou-dans-mon-application
+https://www.skype.com/fr/developer/create-contactme-buttons/
+https://www.skype.com/en/developer/ -->
+
+<a href="skype:live:alexandre?call&video=true">
+<img width="150" height="150" alt="Appel Skype" style="border: none;"src="Alexandre.JPG">
+</a>
+<br/>
+<div class="Legende">
+Appeler Alexandre par Skype
+</div>
+
+</body>
+</html>
+
